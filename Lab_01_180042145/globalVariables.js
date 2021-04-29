d4d9b214d094c29240b0acb28134380d62374e33
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2021-04-30 02:31:39 +0600

    first commit

---
+++ Lab_01_180042145/globalVariables.js
@@ -0,0 +1,14 @@
+//__dirname - path to the current directory
+const currentDirrectory = __dirname;
+console.log(currentDirrectory);
+
+//__filename - file name
+console.log(__filename);
+
+
+//require - function to use modules
+const fs = require("fs");
+console.log(fs);
+
+//module - info about current module
+console.log(module);
