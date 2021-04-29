17d3013f48b964d5182e1e842f7e290d8ec3aa7c
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2021-04-30 02:55:23 +0600

    setInterval and setTimer

--- Lab_01_180042145/utils.js
+++ Lab_01_180042145/utils.js
@@ -1,9 +1,19 @@
 // importing helloWorld.js
 const HelloFunc = require('./helloWorld');
 
-HelloFunc.Hello();
-console.log(HelloFunc.firstName + " " + HelloFunc.lastName);
+//HelloFunc.Hello();
+//console.log(HelloFunc.firstName + " " + HelloFunc.lastName);
 
 
+//interval
+setInterval(() => {
+    HelloFunc.Hello();
+}, 1000);
+
+
+//timeout
+setTimeout(() => {
+    console.log(HelloFunc.firstName, HelloFunc.lastName);
+}, 5000);
 
 //console.log(HelloFunc);
