31402286bb5a7980d8a9fbb1cc6a1fb1d1ccb675
MOHTASIM HADI RAFI <mohtasimhadi@gmail.com>
2021-04-30 13:58:53 +0600

    loadContent done

--- Lab_01_180042145/http-module.js
+++ Lab_01_180042145/http-module.js
@@ -1,11 +1,27 @@
 const http = require("http");
+const fs = require("./fs-module")
 
 const server = http.createServer((req, res) => {
     if(req.url == "/"){
-        res.write("<h1>This is base URL</h1>");
+        res.write(fs.data.index);
         res.end();
-    } else if (req.url == "/home") {
-        res.write("<h1>This is Home Page</h1>")
+    } else if (req.url == "/about") {
+        res.write(fs.data.about);
+        res.end();
+    } else if (req.url == "/blog") {
+        res.write(fs.data.blog);
+        res.end();
+    } else if (req.url == "/contact") {
+        res.write(fs.data.contact);
+        res.end();
+    } else if (req.url == "/pricing") {
+        res.write(fs.data.pricing);
+        res.end();
+    } else if (req.url == "/services") {
+        res.write(fs.data.service);
+        res.end();
+    } else if (req.url == "/work") {
+        res.write(fs.data.work);
         res.end();
     } else {
         res.write("This Page Doesn't Exist<br><a href = '/'> Go To Base </h1>")
