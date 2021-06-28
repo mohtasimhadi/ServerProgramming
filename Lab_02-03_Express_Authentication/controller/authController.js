2266b2dc977ff31bc97334929afcfa0d61e44810
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 21:48:09 +0600

    password and email validation check

--- Lab_02-03_Express_Authentication/controller/authController.js
+++ Lab_02-03_Express_Authentication/controller/authController.js
@@ -1,33 +1,95 @@
-const userSchema = require('../models/userModel')
+const userSchema = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const alert = require('alert');
+
+var LocalStorage = require('node-localstorage').LocalStorage;
+localStorage = new LocalStorage('./scratch');
 
 const getRegister = (req, res) => {
-    res.sendFile("register.html", { root: "./views/users" })
-}
+    res.sendFile("register.html", { root: "./views/users" });
+};
 
-const postRegister = async(req, res) => {
-    const { fullname, email, password, confirmpass } = req.body
+const postRegister = async (req, res) => {
+    const { fullname, email, password, confirmpass } = req.body;
 
-    const newUser = { fullname: fullname, email: email, password: password }
-    console.log(newUser)
+    // new user json 
+    if (String(fullname).length == 0 || String(email).length == 0 || String(password).length == 0 || String(confirmpass).length == 0) {
+        alert('Require all field');
+        res.redirect('/register');
+    }
 
-    const regUser = new userSchema(newUser)
-    regUser.save().then(data => res.json(data)).catch(error => res.json(error))
-}
+    else {
+        const bool = await userSchema.findOne({ email });
+        if (bool) {
+            alert('Email already exist..try again');
+            return res.redirect('/register');
+        }
+
+        if (password.length < 8) {
+            alert('passworld length should be atleast 8');
+            return res.redirect('/register');
+        }
+
+        if (password == confirmpass) {
+            alert('password does not match');
+            return res.redirect('/register');
+        }
+
+        const passwordEncrypted = await bcrypt.hash(password, 10);
+        const newUser = new userSchema({
+            fullname: fullname,
+            email: email,
+            password: passwordEncrypted
+        });
+
+        try {
+            await newUser.save();
+            localStorage.setItem('username', fullname);
+            alert("Registration complete!");
+            res.redirect('/login');
+
+        } catch (error) {
+            res.status(409).json({ message: error.message });
+        }
+
+    }
+};
 
 const getLogin = (req, res) => {
-    res.sendFile("login.html", { root: "./views/users" })
-}
+    res.sendFile("login.html", { root: "./views/users" });
+};
 
-const postLogin = (req, res) => {
-    console.log(req.body)
-}
+const postLogin = async (req, res) => {
+    const { email, password } = req.body;
+
+    const user = await userSchema.findOne({ email });
+    if (!user) {
+        alert('Email not found');
+        return res.redirect('/login');
+    }
+
+    const checkPass = await bcrypt.compare(password, user.password);
+    if (!checkPass) {
+        alert('Password incorrect');
+        return res.redirect('/login');
+    }
+
+    localStorage.setItem('username', user.fullname);
+    return res.redirect('/dashboard');
+};
 
 const getDashboard = (req, res) => {
-    res.sendFile("index.html", { root: "./views"})
-}
+    res.sendFile("index.html", { root: "./views" });
+};
 
 const getHomePage = (req, res) => {
-    res.sendFile("home.html", { root: "./views"})
-}
+    res.sendFile("home.html", { root: "./views" });
+};
 
-module.exports = {getDashboard, getHomePage, getLogin, getRegister, postLogin, postRegister}
+const logout = (req, res) => {
+    res.clearCookie('user');
+    localStorage.removeItem("username");
+    res.redirect('/');
+};
+
+module.exports = { getDashboard, getHomePage, getLogin, getRegister, postLogin, postRegister, logout };
