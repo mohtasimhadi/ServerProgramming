b0fd14c4bcdc6f64a7f238d1d6822b136814adc1
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 20:38:00 +0600

    Changed register.html with template

--- Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
+++ Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
@@ -1,8 +1,15 @@
 const express = require("express")
+const { route } = require("../app")
 const router = express.Router()
 const isLoggedIn = require("../middlewares/auth.middlewares")
 
+router.get("/", (req, res) => {
+    res.sendFile("home.html", { root: "./views"})
+})
 
+router.get("/dashboard", (req, res) => {
+    res.send("<H1>Dashboard</H1>")
+})
 
 router.get("/login", (req, res) => {
     res.sendFile("login.html", { root: "./views/users/" })
@@ -13,8 +20,14 @@ router.get("/register", (req, res) => {
     res.sendFile("register.html", { root: "./views/users/" })
 })
 
-router.post("/register", isLoggedIn, (req, res) => {
-    res.redirect("/dashboard")
+router.post("/", (req, res) => {
+    res.send("<H1>Home Page - POST</H1>")
+})
+
+router.use((req, res) => {
+    res.send(
+        "<H1>Error 404!</H1><br><H2>Page Doesn't Exist</H2>"
+    )
 })
 
 module.exports = router
