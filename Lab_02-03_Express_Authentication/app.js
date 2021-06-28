9118604f538f22bc585910874d5e6db938adb21a
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 11:07:42 +0600

    Initial app.middlewares.js

--- Lab_02-03_Express_Authentication/app.js
+++ Lab_02-03_Express_Authentication/app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const app = express()
 const userRoutes = require("./routes/userRoutes.routes")
+const logger = require("./middlewares/app.middlewares")
 
+app.use(logger)
 app.use(userRoutes)
 
 app.get("/", (req, res) => {
