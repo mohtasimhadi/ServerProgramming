b0a7ed7ebd93aab3864ef07cb97c64daa64fc042
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 21:11:02 +0600

    userRoutes.routes updated

--- Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
+++ Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
@@ -1,28 +1,23 @@
 const express = require("express")
-const { route } = require("../app")
 const router = express.Router()
-const isLoggedIn = require("../middlewares/auth.middlewares")
+const {
+    getDashboard,
+    getHomePage,
+    getLogin,
+    getRegister,
+    postLogin,
+    postRegister
+} = require("../controller/authController")
+const bodyParser = require("body-parser")
 
-router.get("/", (req, res) => {
-    res.sendFile("home.html", { root: "./views"})
-})
+router.use(bodyParser.urlencoded({ extended: false }))
 
-router.get("/dashboard", (req, res) => {
-    res.sendFile("index.html", { root: "./views" })
-})
-
-router.get("/login", (req, res) => {
-    res.sendFile("login.html", { root: "./views/users/" })
-})
-
-
-router.get("/register", (req, res) => {
-    res.sendFile("register.html", { root: "./views/users/" })
-})
-
-router.post("/", (req, res) => {
-    res.send("<H1>Home Page - POST</H1>")
-})
+router.get("/", getHomePage)
+router.get("/dashboard", getDashboard)
+router.get("/login", getLogin)
+router.get("/register", getRegister)
+router.post("/register", postRegister)
+router.post("/login", postLogin)
 
 router.use((req, res) => {
     res.send(
