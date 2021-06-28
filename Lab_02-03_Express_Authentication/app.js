35c6376dbdef03db1e080764817ebd1c3954df65
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 20:19:47 +0600

    connecting db

--- Lab_02-03_Express_Authentication/app.js
+++ Lab_02-03_Express_Authentication/app.js
@@ -2,7 +2,12 @@ const express = require("express")
 const app = express()
 const userRoutes = require("./routes/userRoutes.routes")
 const logger = require("./middlewares/app.middlewares")
-const mongose = require('mongoose')
+const mongoose = require('mongoose')
+
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.error('Connected to Database'))
 
 app.use(express.static('public'))
 app.use(logger)
@@ -14,7 +19,7 @@ app.get("/", (req, res) => {
 
 app.get("/dashboard", (req, res) => {
     res.send(
-        "<H1>Dashboard</H1>"
+        "<H1>Dashboard </H1>"
     )
 })
 
