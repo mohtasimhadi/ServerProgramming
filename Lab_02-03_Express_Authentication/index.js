3f7aa3ca895ec7bbe614973803dd6ebbe43c1bdb
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 20:48:29 +0600

    Connecting Database Revised

--- Lab_02-03_Express_Authentication/index.js
+++ Lab_02-03_Express_Authentication/index.js
@@ -1,7 +1,14 @@
 require("dotenv").config()
 const app = require("./app")
 const PORT = process.env.PORT
+const mongoose = require('mongoose')
 
 app.listen( PORT, ()=> {
     console.log(`Server is running at PORT ${PORT}`)
 })
+
+mongoose.connect(process.env.DATABASE_URL, () => {
+    console.log("Database Connected")
+}).catch(error) => {
+    console.log(error.message)
+}
