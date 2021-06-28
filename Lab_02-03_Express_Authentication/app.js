229d1473089b66f8758405df301bf0a6f4351b65
mohtasimhadi <mohtasimhadi@gmail.com>
2021-06-28 20:37:04 +0600

    Separated userRoutes from app.js

--- Lab_02-03_Express_Authentication/app.js
+++ Lab_02-03_Express_Authentication/app.js
@@ -13,20 +13,4 @@ app.use(express.static('public'))
 app.use(logger)
 app.use(userRoutes)
 
-app.get("/", (req, res) => {
-    res.sendFile("home.html", { root: "./views"})
-})
-
-app.get("/dashboard", (req, res) => {
-    res.send(
-        "<H1>Dashboard </H1>"
-    )
-})
-
-app.use((req, res) => {
-    res.send(
-        "<H1>Error 404!</H1><br><H2>Page Doesn't Exist</H2>"
-    )
-})
-
 module.exports = app
