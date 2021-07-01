2f28857cd802ce4f894d8e208c09e416619c9cc3
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-02 05:41:44 +0600

    DB connection with mongoDB in cloud this time

--- Lab_02-03_Express_Authentication/app.js
+++ Lab_02-03_Express_Authentication/app.js
@@ -1,9 +1,17 @@
+require('dotenv').config()
 const express = require("express")
 const app = express()
 const session = require("express-session")
 const flash = require("connect-flash")
 const userRoutes = require("./routes/userRoutes.routes")
 const indexRoutes = require("./routes/index.routes")
+const moongose = require("mongoose")
+
+moongose.connect(process.env.MongoURI, {useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+    console.log("Connected to DB")
+}).catch((error) => {
+    console.log(error)
+})
 
 app.use(express.static('public'))
 app.use(session({
