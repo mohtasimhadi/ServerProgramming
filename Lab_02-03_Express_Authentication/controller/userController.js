ebc3dc6550baa1d5129b94e1971f63c14606c73e
mohtasimhadi <mohtasimhadi@gmail.com>
2021-07-02 01:55:37 +0600

    userRoutes changed

--- Lab_02-03_Express_Authentication/controller/userController.js
+++ Lab_02-03_Express_Authentication/controller/userController.js
@@ -2,4 +2,20 @@ const pageNotFound = (req, res) => {
     res.send("<H1>Error 404! Page Not Found</H1>")
 }
 
-module.exports = pageNotFound
+const getLogin = (req, res) => {
+    res.render('users/login.ejs')
+}
+
+const postLogin = (req, res) => {
+    
+}
+
+const getRegister = (req, res) => {
+    res.render('users/register.ejs')
+}
+
+const postRegister = (req, res) => {
+    res.render('users/register.ejs')
+}
+
+module.exports = {pageNotFound, getLogin, postLogin, getRegister, postRegister}
--- Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
+++ Lab_02-03_Express_Authentication/routes/userRoutes.routes.js
@@ -1,25 +1,15 @@
 const express = require("express")
 const router = express.Router()
 const {
-    getDashboard,
-    getHomePage,
     getLogin,
     getRegister,
     postLogin,
-    postRegister,
-    logout
-} = require("../controller/authController")
-const bodyParser = require("body-parser")
-const pageNotFound = require("../controller/userController")
-const isLoggedIn = require("../middlewares/auth.middlewares")
+    postRegister
+} = require("../controller/userController")
 
-router.use(bodyParser.urlencoded({ extended: false }))
-
-router.use('/dashboard', isLoggedIn, getDashboard)
-router.get("/logout", logout)
-router.route("/login").get(getLogin).post(postLogin)
-router.route("/register").get(getRegister).post(postRegister)
-
-router.use(pageNotFound)
+router.get('/login', getLogin)
+router.post('/login', postLogin)
+router.get('/register', getRegister)
+router.post('/register', postRegister)
 
 module.exports = router
