a4c0b83ca35fb7054e30b994648e4e50ccd4a83b
mohtasimhadi <mohtasimhadi@gmail.com>
2021-09-02 19:00:03 +0600

    MO Routes Done

--- routes/MathOlympiad.routes.js
+++ routes/MathOlympiad.routes.js
@@ -1,12 +1,15 @@
-const express = require('express')
+const express = require("express")
 const router = express.Router()
-const { ensureAuthenticated, addUserData } = require('../middlewares/auth.middlewares')
-const { getMO, postMO, getMOList, deleteMO, paymentDoneMO } = require('../controller/mathOlympiad.controller')
+const { ensureAuthenticated, addUserData} = require("../middlewares/auth.middleware")
+const { getMO, postMO, getMOList, deleteMO, paymentDoneMO, selectMO, getEditMO, postEditMO} = require("../controllers/mathOlympiad.controller")
 
-router.get('/register', ensureAuthenticated, addUserData, getMO)
-router.post('/register', ensureAuthenticated, addUserData, postMO)
-router.get('/list', ensureAuthenticated, addUserData, getMOList)
-router.get('/delete/:id', ensureAuthenticated, addUserData, deleteMO)
-router.get('/paymentDone/:id', ensureAuthenticated, addUserData, paymentDoneMO)
+router.get("/register", ensureAuthenticated, addUserData, getMO)
+router.post("/register", ensureAuthenticated, addUserData, postMO)
+router.get("/list", ensureAuthenticated, addUserData, getMOList)
+router.get("/delete/:id", ensureAuthenticated, addUserData, deleteMO)
+router.get("/paymentDone/:id", ensureAuthenticated, addUserData, paymentDoneMO)
+router.get("/select/:id", ensureAuthenticated, addUserData, selectMO)
+router.get( "/edit-participant/:id", ensureAuthenticated, addUserData, getEditMO)
+router.post("/edit-participant", ensureAuthenticated, addUserData, postEditMO)
 
 module.exports = router
