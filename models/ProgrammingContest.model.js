a6fa7b3618f98c2f1435db5b5857dfb30ac71bf5
mohtasimhadi <mohtasimhadi@gmail.com>
2021-09-03 13:22:14 +0600

    Programming Contest Model

--- models/ProgrammingContest.model.js
+++ models/ProgrammingContest.model.js
@@ -1,49 +1,97 @@
 const mongoose = require('mongoose')
-const MOSchema = new mongoose.Schema({
-    name:{
+const PCSchema = new mongoose.Schema({
+    teamName: {
         type: String,
-        required: true
-   },
-   registerID: {
-       type: String,
-       required: true,
-   },
-    category:{
-       type: String,
-       required: true
-   },
-   contact: {
-       type: String,
-       required: true,
-   },
-   email:{
-       type: String,
-       required: false
-   },
-   institution:{
-       type: String,
-       required: true
-   },
-   total:{
-       type: Number,
-       required: true
-   },
-   paid:{
-       type: Number,
-       required: true
-   },
-   selected:{
-       type: Boolean,
-       required: true
-   },
-   tshirt:{
-       type: String,
-       required: true
-   },
-   date:{
-       type: Date,
-       default: Date.now
-   },
+        required: true,
+    },
+    institutionName: {
+        type: String,
+        required: true,
+    },
+    coachName: {
+        type: String,
+        required: true,
+    },
+    coachContact: {
+        type: String,
+        required: false,
+    },
+    coachEmail: {
+        type: String,
+        required: true,
+    },
+    coachTshirt: {
+        type: String,
+        required: true,
+    },
+
+    leaderName: {
+        type: String,
+        required: true,
+    },
+    leaderContact: {
+        type: String,
+        required: false,
+    },
+    leaderEmail: {
+        type: String,
+        required: true,
+    },
+    leaderTshirt: {
+        type: String,
+        required: true,
+    },
+
+    member1Name: {
+        type: String,
+        required: true,
+    },
+    member1Contact: {
+        type: String,
+        required: false,
+    },
+    member1Email: {
+        type: String,
+        required: true,
+    },
+    member1Tshirt: {
+        type: String,
+        required: true,
+    },
+
+    member2Name: {
+        type: String,
+        required: true,
+    },
+    member2Contact: {
+        type: String,
+        required: false,
+    },
+    member2Email: {
+        type: String,
+        required: true,
+    },
+    member2Tshirt: {
+        type: String,
+        required: true,
+    },
+
+    total: {
+        type: Number,
+        required: true,
+    },
+    paid: {
+        type: Number,
+        required: true,
+    },
+    selected: {
+        type: Boolean,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
 })
 
 const ProgrammingContest = mongoose.model('ProgrammingContest', PCSchema)
